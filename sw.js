e0879c7c41e8e6278b20191b04dc0b5df8ec0faf
31aprilparker-oss <31aprilparker@gmail.com>
2026-09-21 20:29:14 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,55 @@
-const C='azteca-v4.6.2';
-const A=['./','./index.html','./manifest.webmanifest','./sw.js','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(x=>Promise.all(x.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+const C='azteca-v4.7.1';
+const A=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c=>c.addAll(A)));
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(
+        keys.filter(k=>k!==C).map(k=>caches.delete(k))
+      ))
+      .then(()=>self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(res.ok){const cp=res.clone();caches.open(C).then(c=>c.put(e.request,cp))}return res}).catch(()=>caches.match('./index.html'))));
+
+  const url=new URL(e.request.url);
+
+  if(
+    e.request.mode==='navigate' ||
+    url.pathname.endsWith('/') ||
+    url.pathname.endsWith('/index.html')
+  ){
+    e.respondWith(
+      fetch(e.request)
+        .then(res=>{
+          if(res.ok){
+            const cp=res.clone();
+            caches.open(C).then(c=>c.put(e.request,cp));
+          }
+          return res;
+        })
+        .catch(()=>caches.match(e.request)
+          .then(r=>r||caches.match('./index.html')))
+    );
+    return;
+  }
+
+  e.respondWith(
+    caches.match(e.request).then(r=>
+      r || fetch(e.request).then(res=>{
+        if(res.ok && url.origin===self.location.origin){
+          const cp=res.clone();
+          caches.open(C).then(c=>c.put(e.request,cp));
+        }
+        return res;
+      })
+    )
+  );
 });
